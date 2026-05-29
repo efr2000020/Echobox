ea13e7d86b5224bc4fd562478414edb06214437b
Omar Mokhtar <om.mokhtar.98@gmail.com>
2026-05-29 16:50:02 +0300

    Reduce number of parallel build jobs

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -19,8 +19,10 @@ echo "[1/3] Configuring with CMake (Ninja) in ${BUILD_TYPE} mode..."
 cmake -S . -B "$BUILD_DIR" -G Ninja -DCMAKE_INSTALL_PREFIX="$DEPLOY_DIR" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 
 # Build
+# Single job: the Pi Zero 2 W (512 MB RAM) OOM-kills cc1plus under parallel
+# C++ compiles. We rarely rebuild, so favor reliability over speed.
 echo "[2/3] Building..."
-cmake --build "$BUILD_DIR" -j
+cmake --build "$BUILD_DIR" -j1
 
 # Install/Deploy
 echo "[3/3] Installing to $DEPLOY_DIR..."
